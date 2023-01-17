4a12cc8bf5df57ad705be66a68eee10f94e6d6c5
Mauricio Acuna Navarro <macunan@gmail.com>
2023-01-17 00:52:41 -0300

    Clean Alumnos/Cursos/Clases CRUD

--- src/app/shared/components/clases-dialog/clases-dialog.component.ts
+++ src/app/shared/components/clases-dialog/clases-dialog.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-
+import { DialogRef } from '@angular/cdk/dialog';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Clase} from 'src/app/models/clase.model';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { Component,Inject } from '@angular/core';
 @Component({
   selector: 'app-clases-dialog',
   templateUrl: './clases-dialog.component.html',
@@ -7,4 +10,41 @@ import { Component } from '@angular/core';
 })
 export class ClasesDialogComponent {
 
+  nameControl = new FormControl('')
+  idControl = new FormControl('')
+  cursonameControl = new FormControl('')
+
+
+  claseForm = new FormGroup({
+    name: this.nameControl,
+    cursoname: this.cursonameControl,
+    id:this.idControl
+  })
+
+  constructor(
+    private readonly dialogRef: DialogRef,
+    @Inject(MAT_DIALOG_DATA) public data: Clase | null,
+  ) {
+    if (data) {
+      this.claseForm.patchValue(data)
+
+    }
+  }
+
+  close() {
+    this.dialogRef.close()
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
